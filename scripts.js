971a8e85b68d579c7e40dce7a935089791861191
Rok Slana <rokslana@gmail.com>
2022-09-16 12:26:45 +0200

    Etch A Sketch App

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 const myCanvas = document.querySelector('.container');
-let gridSize = 128;
-let currentCanvasLine = 1;
+let gridSize = 32;
 
-//MAKE CANVAS
-function creatNewCanvas(){
+//CREATE CANVAS
+function createNewCanvas(){
 
     //CREATE NEW CANVAS LINE ...
     for(let i = 1; i<=gridSize; i++){
@@ -18,21 +17,19 @@ function creatNewCanvas(){
             singleCanvasLine.appendChild(singlePixel);
         }
     }
+    //ATTACH EVENTLISTENERS TO ALL PIXELS
+    const canvasPixels = document.querySelectorAll('.pixel');
+    canvasPixels.forEach(pixel => pixel.addEventListener('mouseover', addColor));
 }
 
-creatNewCanvas();
-
-const canvasPixels = document.querySelectorAll('.pixel');
-
-canvasPixels.forEach(pixel => pixel.addEventListener('mouseover', addColor));
-
-
+//PIXEL COLORISATION
 function addColor(){
+
     //GET Px CLASS/NUMBER THAT DEFINES PIXEL COLOR
     let pixelClassIndex = this.classList[1];
     let indexNum = Number(pixelClassIndex.charAt(1));
-    let nextIndexNum;
-    
+    let nextIndexNum;    
+
     //IF NOT MAXED OUT, REMOVE COLOR OPACITY USING CSS CLASSES P1,P2,P3,...P9
     if(indexNum < 9){
         nextIndexNum = indexNum +1;
@@ -41,3 +38,29 @@ function addColor(){
         this.classList.add(nextClass);
     }
 }
+
+
+function resetCanvas(){
+
+    //FIRST REMOVE EXISTING CANVAS
+    while(myCanvas.firstChild){
+        myCanvas.removeChild(myCanvas.firstChild);
+    }
+
+    //CREATE NEW CANVAS, ASK FOR USER INPUT
+    const newCanvasSize = prompt('Enter pixel density for new canvas', '32');
+    if(newCanvasSize >256){
+        prompt('Max canvas density is 256', '256');
+        gridSize = 256;
+    }else{
+        gridSize = newCanvasSize
+    }
+
+    createNewCanvas();
+}
+
+createNewCanvas();
+
+
+
+
